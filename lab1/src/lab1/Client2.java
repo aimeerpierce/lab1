320b740074c2a0a947eae3a552dad2ee19c75430
apierce <aimee@gforgegroup.com>
2016-09-11 16:12:41 -0500

    Added lab1 files

---
+++ lab1/src/lab1/Client2.java
@@ -0,0 +1,294 @@
+package lab1;
+
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client2 {
+	Socket socket;
+	private String username;
+	// Socket serverSocket;
+	String serverHostName = "localhost";
+	int serverPortNumber = 7777;
+	ServerListener sl;
+	String message;
+	Image image = null;
+	boolean isMessage;
+	boolean isAdmin = false;
+
+	Client2(String ipAddr, int serverPort) {
+		// get username
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter a username: ");
+		this.username = in.nextLine().toLowerCase();
+		// in.close();
+		int msgCount = 0;
+		if (username.equals("admin")) {
+			isAdmin = true;
+		}
+
+		PrintWriter out;
+		// connect to server2
+		try {
+			// connect to socket
+			int option = 0;
+			socket = new Socket(ipAddr, serverPort);
+
+			// write username to socket with PrintWriter
+			out = new PrintWriter(this.socket.getOutputStream(), true);
+			out.println(this.username);
+
+			// ask for type of data to send
+			if (isAdmin == false) {
+				System.out.print("Are you sending a message(0) or image(1)? (0/1): ");
+				option = in.nextInt();
+				if (option == 0) {
+					isMessage = true;
+				} else {
+					isMessage = false;
+				}
+
+				// get message as string or image file
+				if (isMessage == true) {
+					System.out.println("Please enter message as a string: ");
+					message = in.next();
+					// send message to Server2 via PrintWriter
+					String encryptedMessage = encrypt(message);
+					msgCount++;
+					//updateTextFile(encryptedMessage, msgCount);
+					System.out.println(encryptedMessage + " " + msgCount);
+					out.println(encryptedMessage);
+					out.println(msgCount);
+					out.flush();
+				} else {
+					System.out.println("Please enter directory path to image. ");
+					String filePath = in.next();
+					FileInputStream fis = new FileInputStream(filePath);
+					byte[] buffer = new byte[fis.available()];
+					fis.read(buffer);
+					String s = encryptImage(buffer);
+					msgCount++;
+					//updateTextFile(s, msgCount);
+					ObjectOutputStream outToServer = new ObjectOutputStream(socket.getOutputStream());
+					outToServer.writeObject(s);
+					outToServer.writeInt(msgCount);
+					outToServer.flush();
+				}
+			} else {
+				System.out.println("Hello admin, please select from the following commands: ");
+				System.out.println("(1) Broadcast message to all clients");
+				System.out.println("(2) List messages in chat.txt");
+				System.out.println("(3) Delete message number in chat.txt");
+				System.out.print("Selection (1/2/3): ");
+				option = in.nextInt();
+			}
+			if (option == 1) {
+				System.out.println("Broadcast message to all clients: ");
+				System.out.print("Enter message: ");
+				String message = in.nextLine();
+			}
+			else if (option == 2) {
+				//Display chat.txt log
+			}
+			else if (option == 3) {
+				System.out.println("Please enter a message number to delete. ");
+				int deleteNum = in.nextInt();
+				deleteMessage(deleteNum);
+				System.out.print("Message "+ deleteNum + " deleted.");
+			}
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// create serverListener
+		sl = new ServerListener(this, this.socket);
+		new Thread(sl).start();
+
+		in.close();
+
+	}
+
+	private static String encryptImage(byte[] bArray) {
+
+		int mask1 = 0xFC;
+		int mask2 = 0x03;
+		int mask3 = 0xF0;
+		int mask4 = 0x0F;
+		int mask5 = 0xC0;
+		int mask6 = 0x3F;
+		int[] numArray = new int[bArray.length];
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < bArray.length - 2; i += 3) {
+
+			// grabbing 3 bytes to turn into 6 bit sections
+			byte b1 = bArray[i];
+			byte b2 = bArray[i + 1];
+			byte b3 = bArray[i + 2];
+
+			/*
+			 * System.out.print("bHex#: " + Integer.toHexString(b1));
+			 * System.out.print(" - " + Integer.toHexString(b2));
+			 * System.out.println(" - " + Integer.toHexString(b3));
+			 */
+
+			// using bit masks to extract the bit sections
+			int val1 = b1 & mask1;
+			int val2 = b1 & mask2;
+			int val3 = b2 & mask3;
+			int val4 = b2 & mask4;
+			int val5 = b3 & mask5;
+			int val6 = b3 & mask6;
+
+			// merging bits into the sections in correct order and position
+			int out1 = val1 >>> 2;
+			int out2 = (val2 << 4) | (val3 >>> 4);
+			int out3 = (val4 << 2) | (val5 >>> 6);
+			int out4 = val6;
+
+			/*
+			 * System.out.println("Encrypt fragments\n" + " --: " +
+			 * bitString(0x00) + " V2: " + bitString(val2) + " V4: " +
+			 * bitString(val4) + " --: " + bitString(0x00) );
+			 * 
+			 * System.out.println("" + " v1: " + bitString(val1) + " v3: " +
+			 * bitString(val3) + " v5: " + bitString(val5) + " v6: " +
+			 * bitString(val6) );
+			 * 
+			 * System.out.println("" + " o1: " + bitString(out1) + " o2: " +
+			 * bitString(out2) + " o5: " + bitString(out3) + " o6: " +
+			 * bitString(out4) );
+			 */
+
+			/*
+			 * System.out.println("Hex #: " + Integer.toHexString(out1) + " - "
+			 * + Integer.toHexString(out2) + " - " + Integer.toHexString(out3) +
+			 * " - " + Integer.toHexString(out4)); sb.append((char) out1);
+			 * sb.append((char) out2); sb.append((char) out3); sb.append((char)
+			 * out4); System.out.print("sHex#: " +
+			 * Integer.toHexString(sb.charAt(0))); System.out.print(" - " +
+			 * Integer.toHexString(sb.charAt(1))); System.out.print(" - " +
+			 * Integer.toHexString(sb.charAt(2))); System.out.println(" - " +
+			 * Integer.toHexString(sb.charAt(3))); System.out.println(
+			 * "Encrypted string: " + sb.toString());
+			 */
+		}
+
+		return sb.toString();
+	}
+
+	private static String encrypt(String input) {
+		String msg = input;
+		char key = 0xF0;
+		String str = Integer.toBinaryString(key);
+		String text = "";
+		int xor;
+		char c, tmp;
+		System.out.println("");
+		for (int i = 0; i < msg.length(); i++) {
+			//System.out.println(i);
+			c = msg.charAt(i);
+			xor = c ^ key;
+			tmp = (char) xor;
+			text = text + tmp;
+		}
+		return text;
+	}
+
+	public static void main(String[] args) {
+		Client2 lc = new Client2("localhost", 7777);
+	}
+
+	class ServerListener implements Runnable {
+		Client2 lc;
+		Scanner in; // this is used to read which is a blocking call
+
+		ServerListener(Client2 lc, Socket s) {
+			try {
+				this.lc = lc;
+				in = new Scanner(new BufferedInputStream(s.getInputStream()));
+				//System.out.println(in.nextLine());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			while (true) { // run forever
+				//System.out.println("Client - waiting to read");
+				//String cmd = in.next();
+				if(in.hasNext()){
+				String s = in.nextLine();
+				lc.handleMessage(s);
+				} 
+				
+			}
+
+		}
+	}
+
+	public void handleMessage(String s) {
+		// switch (cmd) {
+		// case "print":
+		System.out.println("client side: " + s);
+		// break;
+		// case "exit":
+		// System.exit(-1);
+		// break;
+		// default:
+		// System.out.println("client side: unknown command received:" + cmd);
+	}
+
+
+	public static void deleteMessage(int msgIndex) throws IOException {
+
+		String txtFilePath = "chat.txt";
+		File fileName = new File(txtFilePath);
+		File tmpFile = new File("tmp.txt");
+		tmpFile.createNewFile();
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(tmpFile));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Scanner scan = null;
+		try {
+			scan = new Scanner(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine().trim();
+			String index = line.substring(0, line.indexOf(" "));
+			int indexNum = Integer.parseInt(index);
+			if (indexNum == msgIndex) {
+				continue;
+			}
+			try {
+				writer.write(line + System.getProperty("line.separator"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		writer.close();
+		scan.close();
+		tmpFile.renameTo(fileName);
+	}
+}
---
+++ lab1/src/lab1/Server2.java
@@ -0,0 +1,280 @@
+package lab1;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Server2 implements Runnable {
+
+	private ServerSocket serverSocket = null;
+	private Thread mainThread = null;
+	private File file = new File("chat.txt");
+	private PrintWriter writer;
+	private int msgCount;
+	private int port;
+	private String clientName;
+	private ArrayList<ClientHandler> clientList;
+
+	public Server2() {
+		port = 7777;
+		try {
+			// create socket
+			serverSocket = new ServerSocket(port);
+			System.out.println("Server started: " + serverSocket + "At port " + port);
+
+		} catch (IOException ioe) {
+			System.out.println("Can not bind to port " + port + ": " + ioe.getMessage());
+			System.exit(-1);
+		}
+	}
+
+	@Override
+	public void run() {
+		PrintWriter out;
+		Scanner in;
+
+		while (true) {
+			Socket clientSocket = null;
+			try {
+				// wait for client to connect to server
+				clientSocket = serverSocket.accept();
+				//clientList = new ArrayList<String>();
+				in = new Scanner(new BufferedInputStream(clientSocket.getInputStream()));
+
+				// Get clientName from Client2
+				clientName = in.nextLine();
+				//System.out.print(clientSocket.isClosed());
+
+				// spawn thread to handle client request
+				// pass name to client handler
+				Thread t = new Thread(new ClientHandler(clientSocket, clientName));
+				t.start();
+				
+				
+			} catch (IOException e) {
+				System.out.println("Accept failed: " + this.port);
+				System.exit(-1);
+			}
+			//in.close();
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		Server2 server = new Server2();
+		(new Thread(server)).start();
+
+	} // end of main method
+
+} // end of class MyServer
+
+class ClientHandler implements Runnable {
+	Socket s; // this is socket on the server side that connects to Client2
+	int num; // keeps track of its number just for identifying purposes
+	private String clientName;
+	private int msgCount;
+	String[] clients;
+
+	ClientHandler(Socket s, String name) {
+		this.s = s;
+		clientName = name;
+	}
+
+	// This is the client handling code
+	// This keeps running handling client requests
+	// after initially sending some stuff to the client
+	public void run() {
+		System.out.print(s.isClosed());
+		Scanner in;
+		PrintWriter out;
+		int count = 0;
+		String message;
+		try {
+			// 1. GET SOCKET IN/OUT STREAMS
+			in = new Scanner(new BufferedInputStream(s.getInputStream()));
+			out = new PrintWriter(new BufferedOutputStream(s.getOutputStream()));
+
+			// 2. Get clientName
+			while (!in.hasNextLine()) {
+			}
+			message = in.nextLine();
+			msgCount = in.nextInt();
+
+			// PRINT SOME STUFF TO THE CLIENT
+			//out.println("print Hello There"+ clientName);
+			//out.println
+			out.println(("Message: " + message + " Message count: " + msgCount));
+			out.flush();
+			// out.println("print You get three wishes!");
+			// out.flush(); // force the output
+
+			Thread t = new Thread(new ClientThread(s, clientName, message, msgCount));
+			t.start();
+
+			// 3. KEEP LISTENING AND RESPONDING TO CLIENT REQUESTS
+
+			// while (true) {
+			// System.out.println("Server - waiting to read");
+			// while(!in.hasNextLine()){}
+			// String clientMessage = in.nextLine();
+			// handleRequest(clientName,count);
+			// count++;
+			// }
+			// out.println("exit done with wishes");
+			// out.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// This handling code dies after doing all the printing
+	} // end of method run()
+
+	void handleRequest(String s, int count) {
+		clients[count] = s;
+	}
+
+} // end of class ClientHandler
+
+class ClientThread implements Runnable {
+	private String message;
+	private String clientName;
+	private int msgCount;
+	PrintWriter out;
+	Socket socket;
+
+	public ClientThread(Socket s, String name, String msg, int count) {
+		this.message = msg;
+		this.clientName = name;
+		socket = s;
+		msgCount = count;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+
+			out = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
+			out.println("Server received: " + message);
+			out.flush();
+			String decryptedMessage = decrypt(message);
+			out.println("Writing " + decryptedMessage + " to chat2.txt.");
+			out.flush();
+			
+			updateTextFile(decryptedMessage, clientName, msgCount);
+			
+			//out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void updateTextFile(String s, String clientName, int msgCount) {
+
+		// hard code in text file path
+		String txtFilePath = "chat.txt";
+		// File fileName = new File(txtFilePath);
+
+		try (FileWriter fw = new FileWriter(txtFilePath, true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw)) {
+			String msgWithNum = msgCount + " " + clientName + " " + s;
+			out.println(msgWithNum);
+			
+			//fw.close();
+			//bw.close();
+			//out.close();
+		} catch (IOException e) {
+		}
+
+	}
+	private static String decrypt(String input) {
+		String txt = input;
+		char key = 0xF0;
+		String msg = "";
+		int xor;
+		char c, tmp;
+		for (int i = 0; i < txt.length(); i++) {
+			c = txt.charAt(i);
+			xor = c ^ key;
+			tmp = (char) xor;
+			msg = msg + tmp;
+		}
+		return msg;
+	}
+
+	private static String decryptImage(String s) {
+
+		int mask1 = 0x3F; // 0011 1111
+		int mask2 = 0x30; // 0011 0000
+		int mask3 = 0x0F; // 0000 1111
+		int mask4 = 0x3C; // 0011 1100
+		int mask5 = 0x03; // 0000 0011
+		int mask6 = 0x3F; // 0011 1111
+
+		byte b1, b2, b3, b4;
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < s.length() - 3; i += 4) {
+			// System.out.println("s1: "+ s.charAt(i) + "s2: " + s.charAt(i+1) +
+			// "s3: " + s.charAt(i + 2) + "s4: " + s.charAt(i+3));
+			b1 = (byte) s.charAt(i);
+			b2 = (byte) s.charAt(i + 1);
+			b3 = (byte) s.charAt(i + 2);
+			b4 = (byte) s.charAt(i + 3);
+
+			/*
+			 * System.out.print("dHex#: " + Integer.toHexString(b1));
+			 * System.out.print(" - " + Integer.toHexString(b2));
+			 * System.out.print(" - " + Integer.toHexString(b3));
+			 * System.out.println(" - " + Integer.toHexString(b4));
+			 */
+
+			int val1 = b1 & mask1;
+			int val2 = b2 & mask2;
+			int val3 = b2 & mask3;
+			int val4 = b3 & mask4;
+			int val5 = b3 & mask5;
+			int val6 = b4 & mask6;
+
+			// System.out.println("v: " + val1 +" - "+ val2 +" - "+ val3 +" - "+
+			// val4 +" - "+ val5 +" - "+ val6);
+
+			// merging bits into the sections in correct order and position
+			int out1 = (val1 << 2) | (val2 >>> 4);
+			int out2 = (val3 << 4) | (val4 >>> 2);
+			int out3 = (val5 << 6) | val6;
+
+			/*
+			 * System.out.println("Decrypt fragments\n" + " V1: " +
+			 * bitString(val1) + " V3: " + bitString(val3) + " V5: " +
+			 * bitString(val5) + "\n" + " v2: " + bitString(val2) + " v4: " +
+			 * bitString(val4) + " v6: " + bitString(val6) + "\n" + " o1: " +
+			 * bitString(out1) + " o2: " + bitString(out2) + " o3: " +
+			 * bitString(out3));
+			 */
+
+			System.out.println("Hexes: " + out1 + " -- " + out2 + " -- " + out3);
+			char c1 = (char) out1;
+			char c2 = (char) out2;
+			char c3 = (char) out3;
+			sb.append(c1);
+			sb.append(c2);
+			sb.append(c3);
+			// System.out.println("c1: " + c1 + "c2: " + c2 + "c3: " + c3);
+			// System.out.println("Decrypting output in progress: " +
+			// sb.toString());
+		}
+		return sb.toString();
+	}
+} // end of class ClientThread
